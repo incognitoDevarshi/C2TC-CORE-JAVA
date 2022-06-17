fb285b179624766222238242179d6eefa6f001a7
chinusavant <chinmaysavant@sjcem.edu.in>
2022-06-17 09:09:53 +0530

    second commit

---
+++ C2TC CORE JAVA TRANING/src/com/inheritance/java/Inherit.java
@@ -0,0 +1,13 @@
+package com.inheritance.java;
+
+import com.packeges.java.FlowStatement;
+
+public class Inherit {
+
+	public static void main(String[] args) {
+		
+		FlowStatement FS=new FlowStatement();
+		FS.IfElse();
+	}
+
+}
