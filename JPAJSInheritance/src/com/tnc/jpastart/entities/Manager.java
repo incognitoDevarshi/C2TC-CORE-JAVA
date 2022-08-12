8e1f995b785008817449979ff62aa3463358d1e3
chinusavant <chinmaysavant@sjcem.edu.in>
2022-08-12 08:50:28 +0530

    push

---
+++ JPAJSInheritance/src/com/tnc/jpastart/entities/Manager.java
@@ -0,0 +1,21 @@
+package com.tnc.jpastart.entities;
+
+@Entity
+@Table(name="mgr_store")
+public class Manager extends Employee {
+
+	private static final long serialVersionUID = 1L;
+	
+	private String departmentName;
+
+	public String getDepartmentName() {
+		return departmentName;
+	}
+
+	public void setDepartmentName(String departmentName) {
+		this.departmentName = departmentName;
+	}
+	
+	
+
+}
